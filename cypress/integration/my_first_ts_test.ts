687d1379dc32ace977287e50dc83eab0333eaab0
Michael Welsch <klein_michi@gmx.net>
2020-11-29 13:09:18 +0100

    added cypress and first example tests

---
+++ cypress/integration/my_first_ts_test.ts
@@ -0,0 +1,115 @@
+describe('My First Test', () => {
+
+  // todo: move to PO of specific page or cypress command
+  const navigateTo = (linkLabel: string) => {
+
+    cy.get('mat-sidenav').then(($nav) => {
+      // jQuery
+      if (!$nav.find(linkLabel).length) {
+        cy.contains('Gruppen Link').click();
+      }
+      cy.contains(linkLabel).click();
+    });
+  };
+
+  beforeEach(() => {
+
+    cy.visit('http://localhost:4200/');
+
+  });
+
+  it('Visits the staring page', () => {
+
+    cy.contains('material-playground');
+
+    cy.get('mat-sidenav')
+      .as('sidenav')
+      .not('Page 2');
+
+    cy.get('@sidenav').not('Page 3');
+
+    cy.contains('Gruppen Link').click();
+
+    cy.get('@sidenav')
+      .contains('Page 2');
+    cy.get('@sidenav')
+      .contains('Page 3');
+
+    cy.contains('Page 2');
+    cy.contains('Page 3');
+  });
+
+  it('clicking example navigates to a new url', () => {
+
+    cy.url().should('include', '/table');
+
+    cy.contains('example').click();
+
+    // Should be on a new URL which includes '/commands/actions'
+    cy.url().should('include', '/example');
+  });
+
+  it('Gets, types and asserts input field', () => {
+
+    navigateTo('Page 2');
+
+    cy.contains('Legacy form field');
+
+    // approach 1:
+    cy.get('mat-form-field')
+      .first()
+      .type('legacy');
+    // .should('have.value', 'legacy');
+
+    cy.get('input')
+      .first()
+      .should('have.value', 'legacy');
+
+    // approach 2:
+    cy.get('input')
+      .first()
+      .clear()
+      .type('newValue')
+      .should('have.value', 'newValue');
+
+    // approach 3:
+    cy.get('[data-cy=legacy-input]')
+      .clear()
+      .type('third')
+      .should('have.value', 'third');
+  });
+
+  it('test tree page', () => {
+
+    navigateTo('tree');
+
+    cy.contains('src').should('not.exist');
+    cy.contains('cdk').should('not.exist');
+    cy.contains('package.json').should('not.exist');
+
+    cy.get('mat-tree-node button')
+      .first()
+      .click();
+
+    cy.contains('src');
+    cy.contains('cdk').should('not.exist');
+    cy.contains('package.json').should('not.exist');
+
+    cy.get('mat-tree-node button')
+      .eq(1)
+      .click();
+
+    cy.contains('src');
+    cy.contains('cdk');
+    cy.contains('package.json').should('not.exist');
+
+    cy.get('mat-tree-node button')
+      .eq(2)
+      .click();
+
+    cy.contains('src');
+    cy.contains('cdk');
+    cy.contains('package.json');
+  });
+
+});
